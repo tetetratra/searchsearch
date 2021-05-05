51fcb478c0bd320441fce329c21b58b3cbe55773
tetetratra <kurutoga.kingyo@gmail.com>
2021-05-05 22:49:56 +0900

    QueryStirngController#index を書いた

--- server/app/controllers/query_strings_controller.rb
+++ server/app/controllers/query_strings_controller.rb
@@ -1,7 +1,34 @@
 class QueryStringsController < ApplicationController
   # before_action :auth_user, only: [:create, :destroy]
   def index
-    queryStrings = QueryString.all
+    # <ActionController::Parameters {"query"=>"goo", "sort"=>"Star", "prefixMatch"=>"true", "distinct"=>"true", "onlyStar"=>"true", "author"=>"てすと", "controller"=>"query_strings", "action"=>"index", "query_string"=>{}} permitted: false>
+    queryStrings = QueryString
+      .select(<<~SQL
+        query_strings.*,
+        users.*,
+        COUNT(favorites.id) AS favorite_count
+      SQL
+      )
+      .joins(:user)
+      .joins('LEFT OUTER JOIN favorites ON favorites.query_strings_id = query_strings.id')
+      .then { |r|
+        query = params[:query]
+        query_like = params[:prefixMatch] ? "#{query}%" : "%#{query}%"
+        query ? r.where('path LIKE ?', query_like) : r
+      }
+      .then { |r|
+        case params[:sort]
+        when 'Star' then r.order('favorite_count DESC')
+        when 'New' then r.order(:created_at)
+        when 'Old' then r.order(created_at: :desc)
+        else r.order('favorite_count DESC')
+        end
+      }
+      .then { |r|
+        author = params[:author]
+        author ? r.where('users.name = ?', author) : r
+      }
+      .group('query_strings.id')
     render json: queryStrings
   end
 
