38379db8f4d14f953e536d31deac08b2a6e91c0e
tetetratra <44367208+tetetratra@users.noreply.github.com>
2021-06-14 20:27:21 +0900

    RailsのAPIを解除してcookieを使えるようにした

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
   rescue_from StandardError do |e|
+    puts e.full_message if Rails.env == 'development'
+
     render json: { error: '内部エラーが起きました' }, status: :unprocessable_entity
   end
 end
