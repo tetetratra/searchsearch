b38c6159278c6eb730cb91c33aeff78359bfd92e
tetetratra <44367208+tetetratra@users.noreply.github.com>
2021-07-08 18:47:21 +0900

    packs/js と assets もサーバーに問い合わせるようにcreateProxyMiddlewareプロキシを修正

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,7 +3,7 @@ const createProxyMiddleware = require('http-proxy-middleware');
 // package.jsosnに`"proxy": "http://server:5000",`と書いていたのをこちらに移動
 module.exports = function(app) {
   app.use(
-    /\/api|\/rails|\/users/,
+    /\/api|\/rails|\/users|\/packs\/js|\/assets/,
     createProxyMiddleware({
       target: 'http://server:5000',
       changeOrigin: true,
